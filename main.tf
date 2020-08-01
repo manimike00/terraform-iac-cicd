100992ff0fae8f236bff96f35fb61ca9f6f9970d
manimike00 <manimike00@gmail.com>
2020-08-02 04:48:15 +0530

    add state

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-
+terraform {
+  backend "s3" {}
+}
 
 module "vpc" {
-  source = "./vpc"
+  source = "./modules/vpc"
   region = var.region
   profile = var.profile
   name_prefix = var.name_prefix
@@ -10,11 +12,10 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
 }
 
-module "ekscluster" {
-  //depends_on =
-  region = var.region
-  profile = var.profile
-  source = "./ekscluster"
-  cluster_name = var.cluster_name
-  subnet_ids = module.vpc.public_subnets
-}
+//module "ekscluster" {
+//  region = var.region
+//  profile = var.profile
+//  source = "./modules/ekscluster"
+//  cluster_name = var.cluster_name
+//  subnet_ids = module.vpc.public_subnets
+//}
